6569461d3aa192cda85fcb889924ad87d7c70165
MfrMesa <berriesccs@gmail.com>
2024-05-14 18:02:09 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from './Root'
-import { Analytics } from "@vercel/analytics/react"
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Analytics>
       <Root />
-    </Analytics>
   </React.StrictMode>,
 )
